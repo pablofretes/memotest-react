0d557f15e9547d8854e212b7256f485bc84a3c08
pablofretes <pablofretes.balzarelli@gmail.com>
2022-01-28 21:09:40 -0300

    Replay Modal style

--- frontend/src/components/ReplayModal.js
+++ frontend/src/components/ReplayModal.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { Box, Typography, Modal } from '@material-ui/core';
+import { Box, Typography, Modal, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+    text: {
+        fontSize: 20,
+        fontFamily: 'Cairo',
+        fontWeight: 'bolder'
+    }
+}))
 
 const ReplayModal = ({ open, setOpen, reset }) => {
+    const classes = useStyles();
     const handleClose = () => setOpen(false);
 
     return(
         <div>
             <Modal aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description" open={open} onClose={handleClose} data-cy="replay-modal">
-                <Box className="styleBox">
-                    <Typography id="modal-modal-title" align="center" className="text-modal">
+                <Box className="style-box">
+                    <Typography id="modal-modal-title" align="center" className={classes.text}>
                         You Win! Do You Want To Play Again?
                     </Typography>
-                    <br></br>
-                    <Typography variant="button" align="center" color="primary">
-                        <button onClick={reset} align="center" className="play-again-button">Play</button>
-                    </Typography>
+                    <button onClick={reset} align="center" className="play-again-button">Play</button>
                 </Box>
             </Modal>
         </div>
