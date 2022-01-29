d1d21e6c1a4a341fd9ecdc3610ec836d3707f5a4
pablofretes <pablofretes.balzarelli@gmail.com>
2022-01-29 18:47:41 -0300

    correcting import
    AppBar's Toolbar component was being imported incorrectly

--- frontend/src/components/AppBar.js
+++ frontend/src/components/AppBar.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
-import ToolBar from '@material-ui/core/ToolBar';
+import Toolbar from '@mui/material/Toolbar';
 import { Link } from 'react-router-dom';
 import { useApolloClient } from '@apollo/client';
 
@@ -36,7 +36,7 @@ const AppBarMemotest = ({ token, setToken }) => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" className={classes.appBar} component='div'>
-        <ToolBar variant="dense" className={classes.toolBar} component='div'>
+        <Toolbar variant="dense" className={classes.toolBar} component='div'>
             <Button component={Link} to="/" data-cy="home-button" className={classes.button}>
               Home
             </Button>
@@ -53,7 +53,7 @@ const AppBarMemotest = ({ token, setToken }) => {
             {token === null && (
               <Button component={Link} to="/signUp" data-cy="signUp-button" className={classes.button}>Sign Up</Button>
             )}
-        </ToolBar>
+        </Toolbar>
       </AppBar>
     </Box>
   );
