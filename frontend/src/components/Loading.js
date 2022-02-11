d3d37e36ab8ea27f863168fb40485810a08e7942
Pablo Fretes <pablofretes.balzarelli@gmail.com>
2022-02-11 02:29:39 -0300

    Styles to app.css
    makeStyles was unnecesary

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
 import loading from '../utils/pokeimages/loading-transparent.gif';
 
-const useStyles = makeStyles(() => ({
-    parent: {
-        display: 'flex',
-        justifyContent: 'center',
-        margin: 'auto',
-    },
-    gif: {
-        height: 50,
-        width: 50,
-        margin: 'auto',
-    }
-}))
-
 const Loading = () => {
-    const classes = useStyles();
     return(
-        <div className={classes.parent}>
-            <img className={classes.gif} src={loading} alt="Loading gif" data-cy="loading-gif"/>
+        <div className='loading-parent'>
+            <img className='loading-gif' src={loading} alt="Loading gif" data-cy="loading-gif"/>
         </div>
     );
 };
